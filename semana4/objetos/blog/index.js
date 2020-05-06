29abe6f1bd14ffc22ea333895f3da9c3276e8a55
FePenedo <fdm.adm@gmail.com>
2020-05-06 17:15:00 -0300

    Adicionei arquivos do projeto blog

---
+++ semana4/objetos/blog/index.js
@@ -0,0 +1,6 @@
+function inserirDadosBotao() {
+    
+let autor = document.getElementById('autor').value;
+let titulo = document.getElementById('titulo').value;
+let conteudoBlog = document.getElementById('conteudoBlog').value;
+}

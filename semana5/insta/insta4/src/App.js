18a874254bb91f56723c33bd6280f935a69dea35
FePenedo <fdm.adm@gmail.com>
2020-05-14 23:26:56 -0300

    Adicionado parte de transformacao da funçoes em classe

--- semana5/insta/insta4/src/App.js
+++ semana5/insta/insta4/src/App.js
@@ -1,38 +1,46 @@
 import React from 'react';
-
 import Post from './components/Post/Post';
+import styled from 'styled-components'
+import { SecaoComentario } from './components/SecaoComentario/SecaoComentario';
 
 class App extends React.Component {
   state = {
-    componente: [
+    post: [
          {   
-        nomeUsuario: "ave",
+          nomeUsuario: "ave",
           fotoUsuario: "https://picsum.photos/50/50",
           fotoPost: "https://picsum.photos/200/150"
     },
-
- 
-
+  {
+    nomeUsuario: "planicie",
+    fotoUsuario: "https://picsum.photos/50/51",
+    fotoPost: "https://picsum.photos/200/151"
+  },
+{
+          nomeUsuario: "cachoeira",
+          fotoUsuario: "https://picsum.photos/50/52",
+          fotoPost: "https://picsum.photos/200/152"
+},
+    ]
   }
   render() {
+    const listaDePosts = this.state.post.map(comentario => {
     return (
-      <div className={'app-container'}>
-        <Post
-          {nomeUsuario={'ave'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-    }
-        />
-        <Post
-          nomeUsuario={'planicie'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
-        <Post
-          nomeUsuario={'cachoeira'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
+          <Post
+          nomeUsuario={comentario.nomeUsuario}
+          fotoUsuario={comentario.fotoUsuario}
+          fotoPosto={comentario.fotoUsuario}
+      />
+
+        );
+      });
+ 
+    return (
+      <div className={"app-container"}>
+        <h1>Post</h1>
+        {/* Como sempre, colocamos a variável com a lista de componentes
+        gerados aqui. */}
+        <div>{listaDePosts}</div>
       </div>
     );
   }
